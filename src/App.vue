3f7c683babb66cf6a51ed349acc452532e15d829
igor.prokofiev <igorprodev1@gmail.com>
2020-10-13 19:07:52 +0300

    demonstration

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h1>Loading: {{loading}}</h1>
+  <hr />
+  <button @click="onToggLoading">Toggle loading</button>
 </template>
 
 <script lang="ts">
@@ -7,13 +9,26 @@ import { Vue } from 'vue-class-component';
 
 export default class App extends Vue {
   loading = false
+
+  onToggLoading() {
+    this.loading=!this.loading
+    console.log('this.loading', this.loading)
+  }
+
   mounted () {
-    setTimeout(() =>{
+    setTimeout(() => {
       this.loading = true
+      console.log('this.loading',this.loading)
     }, 1000)
-    setTimeout(() =>{
+    setTimeout(() => {
       this.loading = false
+      console.log('this.loading',this.loading)
     }, 2000)
+  }
+  watch = {
+    loading (newV: boolean , oldV: boolean) {
+      console.log('loading', newV, oldV)
+    }
   }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+console.log('App', App)
+const app = createApp(App)
+console.log('app options', (app as any).$options)
+app.mount('#app')
